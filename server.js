5ce31427376321dc1d7233c4c3dcbae97c5cc9ce
WeerapatFirst <weerapatyfirst@gmail.com>
2024-06-19 14:33:18 +0700

    get-user

--- server.js
+++ server.js
@@ -3,18 +3,19 @@ const notFound = require("./middleware/notFound");
 require("dotenv").config();
 const app = express();
 
-<<<<<<< Updated upstream
 app.use("/register", (req, res) => {
   res.json({ message: "register..." });
 });
 
 app.use(notFound);
 
-=======
 app.use("/login", (req, res) => {
   res.json({ msg: "Login successfully" });
 });
 
->>>>>>> Stashed changes
+app.use("/get-user", (req, res) => {
+  res.status(401).json({ message: "GET USER..." });
+});
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log("server on ", port));
