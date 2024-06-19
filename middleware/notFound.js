b810fc90ab4c1024580bbe579c104d88747e29a9
supakorn roekratchaneekorn <gongsup@supakornsLaptop.home>
2024-06-19 12:27:40 +0700

    notfound ok

---
+++ middleware/notFound.js
@@ -0,0 +1,5 @@
+const notFound = (req, res) => {
+  res.send("notfound");
+};
+
+module.exports = notFound;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const notFound = require("./middleware/notFound");
 require("dotenv").config();
 const app = express();
+
+app.use(notFound);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log("server on ", port));
